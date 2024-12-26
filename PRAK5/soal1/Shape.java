820321e3b88e7c8ace549d2db73c86d6a5dcd50e
Adrian Bintang Saputera <129594599+MadeByBintang@users.noreply.github.com>
2024-12-26 15:11:54 +0700

    Add files via upload

---
+++ PRAK5/soal1/Shape.java
@@ -0,0 +1,12 @@
+package soal1;
+
+public abstract class Shape {
+    private String shapeName;
+    public Shape(String shapeName) {
+        this.shapeName = shapeName;
+    }
+    public abstract double area();
+    public String toString(){
+        return this.shapeName;
+    }
+}
